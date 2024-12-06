9c3710a0f53b96eb223494364bcc2cfaeb91dab5
Satyam Yadav <167583328+SimpleCyber@users.noreply.github.com>
2024-12-07 02:29:49 +0530

    Update insta.py

--- insta.py
+++ insta.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 CORS(app)
 
 # Load API key from environment variable
-RAPIDAPI_KEY = os.getenv("RAPIDAPI_KEY")
+RAPIDAPI_KEY = "d65eb81e02mshd8f1eca29ba52b7p17caeajsn661881ae5f2c"
 if not RAPIDAPI_KEY:
     raise EnvironmentError("RAPIDAPI_KEY is not set. Please set it in your environment variables.")
 
